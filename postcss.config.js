a37f96c5736f96c05c9d64059fa4d59506f89fb2
miinje <njjj00083@gmail.com>
2023-06-22 09:28:13 +0900

    postcss build 명령어 사용

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,6 @@ const postcss = require("postcss");
 module.exports = {
   plugins: [
     postcss([require("cssnano"), require("postcss-combine-media-query")]),
+    // postcss([require("postcss-combine-media-query")]), css 나노로 만들지 않음
   ],
 };
